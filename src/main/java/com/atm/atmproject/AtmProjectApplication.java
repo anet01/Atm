2133440398b7aee190159761e202ddecf25cd98f
Berkay <berkaybahceci@gmail.com>
2022-07-31 16:50:34 +0300

    First Commit

---
+++ src/main/java/com/atm/atmproject/AtmProjectApplication.java
@@ -0,0 +1,22 @@
+package com.atm.atmproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+////Security Inactive
+//@SpringBootApplication(exclude = {
+//        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+//        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class})
+
+//Auditing
+//Kim ne zaman ekledi
+//@EnableJpaAuditing(auditorAwareRef = "auditorAware")
+public class AtmProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtmProjectApplication.class, args);
+    }
+
+}
